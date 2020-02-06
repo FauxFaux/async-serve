598db90ca1b346549b30dfe76f4543006915895c
Chris West (Faux) <git@goeswhere.com>
2020-02-06 20:52:39 +0000

    feat: intermediate dropper

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use std::pin::Pin;
+
 use async_std::net::TcpListener;
 use async_std::net::TcpStream;
 use async_std::stream::StreamExt as _;
 use async_std::task;
+use futures::future::FusedFuture;
 use futures::io::AsyncReadExt as _;
 use futures::io::AsyncWriteExt as _;
+use futures::stream::FuturesUnordered;
+use futures::stream::Stream as _;
+use futures::task::Context;
+use futures::task::Poll;
+use futures::Future;
 use futures::FutureExt;
 
 fn main() {
@@ -19,7 +27,9 @@ async fn run() {
     let mut incoming = serv.incoming();
     let mut stopper = async_ctrlc::CtrlC::new().expect("init").fuse();
 
-    let mut workers = Vec::new();
+    let mut workers = Unordered {
+        inner: Box::pin(FuturesUnordered::new()),
+    };
 
     loop {
         let client = futures::select! {
@@ -28,6 +38,7 @@ async fn run() {
                 break;
             },
             client = incoming.next().fuse() => client,
+            _ = workers => continue,
         };
 
         // I think this might be irrefutable.
@@ -39,18 +50,38 @@ async fn run() {
         let client = client.expect("accept");
         log::info!("accepted client from {:?}", client.peer_addr());
         let handle = task::spawn(client_loop(client));
-        workers.push(handle);
+        workers.inner.push(handle);
     }
 
     drop(serv);
     log::info!("listener shut down, draining clients");
 
-    for (id, worker) in workers.into_iter().enumerate() {
-        log::info!("draining client {}", id);
-        worker.await;
+    while let Some(_result) = workers.inner.next().await {
+        log::info!("drained worker");
     }
 
     log::info!("listener shut down, bye");
+}
+
+struct Unordered<T, Fut: Future<Output = T>> {
+    inner: Pin<Box<FuturesUnordered<Fut>>>,
+}
+
+impl<T, Fut: Future<Output = T>> Future for Unordered<T, Fut> {
+    type Output = T;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.inner.as_mut().poll_next(cx) {
+            Poll::Ready(Some(x)) => Poll::Ready(x),
+            Poll::Ready(None) | Poll::Pending => Poll::Pending,
+        }
+    }
+}
+
+impl<T, Fut: Future<Output = T>> FusedFuture for Unordered<T, Fut> {
+    fn is_terminated(&self) -> bool {
+        false
+    }
 }
 
 async fn client_loop(mut conn: TcpStream) {
